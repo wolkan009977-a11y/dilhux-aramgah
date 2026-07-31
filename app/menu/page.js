ead99d7e7568b226c69d0d3df59701d181a2d4e9
wolkan009977-a11y <wolkan009977@gmail.com>
2026-07-31 10:09:41 +0300

    Update page.js

--- app/menu/page.js
+++ app/menu/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { db } from "@/lib/firebase";
+import { db } from "../../lib/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 export default function Menu() {
